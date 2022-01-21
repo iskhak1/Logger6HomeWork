33929cd34a0c7a0582262fd7da71ee8b990aa2c7
Iskhak <isakasi12242001@gmail.com>
2022-01-21 22:11:56 +0300

    create method2. Test for method2 is done

--- src/ru/gb/Main.java
+++ src/ru/gb/Main.java
@@ -5,41 +5,57 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int [] array = { 1, 2,  2 ,3 ,4, 8, 1, 7, 9, 8, 2};
-     new Main().method1(array);
+        int[] array = {1, 1, 4, 4, 1, 4};
+        new Main().method2(array);
     }
 
-    public int[] method1(int[] array ){
+    public int[] method1(int[] array) {
 
         int helper = 0;
         boolean key = true;
 
         for (int i = 0; i < array.length; i++) {
-            if(array[i] == 4){
+            if (array[i] == 4) {
                 key = false;
             }
         }
 
         for (int i = 0; i < array.length; i++) {
 
-            if(key == true){
+            if (key == true) {
                 throw new RuntimeException();
             }
 
-            if(array[(array.length-1)-i] != 4 ||  (array[(array.length-1)-i] == 8 )){
+            if (array[(array.length - 1) - i] != 4 || (array[(array.length - 1) - i] == 8)) {
                 helper++;
-            }else{
+            } else {
                 break;
             }
 
         }
-        int [] arrayToReturn = new int[helper];
+        int[] arrayToReturn = new int[helper];
         for (int i = 0; i < helper; i++) {
 
-            arrayToReturn[i] = array[((array.length-1)-helper+1) + i];
+            arrayToReturn[i] = array[((array.length - 1) - helper + 1) + i];
         }
         System.out.println(Arrays.toString(arrayToReturn));
         return arrayToReturn;
     }
 
+
+    public boolean method2(int[] array) {
+        boolean bool = false;
+        //{  4,4,4,4,4,5}
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 1 || array[i] == 4) {
+                if (array[i] != 4) {
+                    bool = true;
+
+                }
+            }
+        }
+        System.out.println(bool);
+        return bool;
+    }
 }
+
